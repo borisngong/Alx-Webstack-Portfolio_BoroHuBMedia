861353c6462b690f6802fd9c4fd3c81ea6114547
borisngong <borisngong@gmail>
2025-01-03 03:57:34 -0800

    Define Post model: author, content, media, likes, comments, and like count

---
+++ BoroHubMedia_Backend/coreModels/contentPost.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Member",
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    media: [
+      {
+        type: String,
+        default: [],
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Member",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+// Create the model for the Post schema
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
