3110b8b87c4d1e1fbcaa646824abcbb6527918ea
borisngong <borisngong@gmail>
2025-01-06 06:14:42 -0800

    Updated auth routes implementation  for member login, logout, and token generation

--- BoroHubMedia_Backend/_bd_api/authSessionRoutes.js
+++ BoroHubMedia_Backend/_bd_api/authSessionRoutes.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const UserController = require('../controllers/authSessionControllers');
+const express = require("express");
+const MemberController = require("../controllers/authSessionControllers");
 
 const router = express.Router();
 
 // Member Auth Route
-router.post('/initializeAccount', (req, res) => {
-  UserController.initializeAccount(req, res);
+router.post("/initializeAccount", (req, res) => {
+  MemberController.initializeAccount(req, res);
 });
-router.post('/accessAccount', (req, res) => {
-  UserController.accessAccount(req, res);
+router.post("/accessAccount", (req, res) => {
+  MemberController.accessAccount(req, res);
 });
-router.get('/endSession', (req, res) => {
-  UserController.endSession(req, res);
+router.get("/endSession", (req, res) => {
+  MemberController.endSession(req, res);
 });
-router.get('/getUserSession', (req, res) => {
-  UserController.getUserSession(req, res);
+router.get("/getUserSession", (req, res) => {
+  MemberController.getUserSession(req, res);
 });
 
 module.exports = router;
