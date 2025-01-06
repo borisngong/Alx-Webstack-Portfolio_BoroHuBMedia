c1198db7c15f2d715a3969777f6ea84b5b0901c6
borisngong <borisngong@gmail>
2025-01-06 05:15:51 -0800

    Sets up moongo DB database connection with error handling

--- BoroHubMedia_Backend/configurations/databaseSetup.js
+++ BoroHubMedia_Backend/configurations/databaseSetup.js
@@ -1,11 +1,16 @@
-const mongoose = require("mongoose");
+/**
+ * Establishes a connection to the MongoDB database using Mongoose
+ *
+ * @returns {Promise<void>} A promise that resolves when the DB connection is established
+ */
+const mongoose = require('mongoose');
 
 const boroHubMediaAPIDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI);
-    console.log("Database connected successfully");
+    console.log('Database connected successfully');
   } catch (error) {
-    console.error("Database connection failed:", error);
+    console.error('Database connection failed:', error);
     process.exit(1);
   }
 };
