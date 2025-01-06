f39992b1dbc4945a4a11838399b1cfdad12613e8
borisngong <borisngong@gmail>
2025-01-06 05:24:25 -0800

    Environment loader which sets up and initializes environment variables

--- BoroHubMedia_Backend/configurations/environmentLoader.js
+++ BoroHubMedia_Backend/configurations/environmentLoader.js
@@ -1,8 +1,18 @@
-const dotenv = require("dotenv");
+/**
+ * responsible for loading environment variables from a .env file
+ *
+ * @returns {void}
+ */
+const dotenv = require('dotenv');
 
 const setupEnvironment = () => {
-  dotenv.config();
-  console.log("Environment variables loaded");
+  try {
+    dotenv.config();
+    console.log('Environment variables loaded');
+  } catch (error) {
+    console.error('Failed to load environment variables:', error);
+    process.exit(1);
+  }
 };
 
 module.exports = setupEnvironment;
