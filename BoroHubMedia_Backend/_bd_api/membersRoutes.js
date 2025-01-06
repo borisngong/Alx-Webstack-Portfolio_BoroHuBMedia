1ed46eb3be4601726e5c8ea11572e2c160c131c4
borisngong <borisngong@gmail>
2025-01-06 05:04:02 -0800

    Updated  member management routes to follow, unfollow, restrict, delete, and update profile images

--- BoroHubMedia_Backend/_bd_api/membersRoutes.js
+++ BoroHubMedia_Backend/_bd_api/membersRoutes.js
@@ -1,85 +1,91 @@
-const express = require("express");
-const MemberController = require("../controllers/memberControllers");
-const { authenticateToken, isAdmin } = require("../middlewares/authIsAdmin");
-const resources = require("../middlewares/mediaUploads");
+const express = require('express');
+const MemberController = require('../controllers/memberControllers');
+const { authenticateToken, isAdmin } = require('../middlewares/authIsAdmin');
+const resources = require('../middlewares/mediaUploads');
 
 const router = express.Router();
 
-// Delete member with admin privileges
-router.delete("/delete/:memberId", authenticateToken, isAdmin, (req, res) => {
+// Delete a member with admin privileges
+router.delete('/delete/:memberId', authenticateToken, isAdmin, (req, res) => {
   MemberController.adminDeleteMember(req, res);
 });
 
-// Get member details
-router.get("/:memberId", (req, res) => {
+// Retrieve a member's details
+router.get('/:memberId', (req, res) => {
   MemberController.getMember(req, res);
 });
 
-// Update member details
-router.put("/update/:memberId", (req, res) => {
+// Update a member's details
+router.put('/update/:memberId', (req, res) => {
   MemberController.updateMember(req, res);
 });
-// delete member details
-router.delete("/delete/:memberId", (req, res) => {
+
+// Delete a member's details
+router.delete('/delete/:memberId', (req, res) => {
   MemberController.deleteMember(req, res);
 });
-// follow member
-router.put("/follow/:memberId", (req, res) => {
+
+// Follow a member
+router.put('/follow/:memberId', (req, res) => {
   MemberController.followMember(req, res);
 });
 
-// Unfollow member
-router.delete("/unfollow/:memberId", (req, res) => {
+// Unfollow a member
+router.delete('/unfollow/:memberId', (req, res) => {
   MemberController.unfollowMember(req, res);
 });
 
-// Get followers list
-router.get("/followers/:memberId", (req, res) => {
+// Get a list of followers
+router.get('/followers/:memberId', (req, res) => {
   MemberController.getFollowersList(req, res);
 });
 
-// Restrict user route
-router.post("/restricted/:memberId", (req, res) => {
+// Restrict a user
+router.post('/restricted/:memberId', (req, res) => {
   MemberController.restrictedMember(req, res);
 });
 
-// unrestricted user route
-router.delete("/unrestricted/:memberId", (req, res) => {
+// Unrestrict a user
+router.delete('/unrestricted/:memberId', (req, res) => {
   MemberController.unrestrictedMember(req, res);
 });
-// Get blocking list
-router.get("/restricting/:memberId", (req, res) => {
+
+// Get a list of blocked users
+router.get('/restricting/:memberId', (req, res) => {
   MemberController.getRestrictedList(req, res);
 });
 
-// Delete member
-router.delete("/delete/:memberId", (req, res) => {
+// Delete a member
+router.delete('/delete/:memberId', (req, res) => {
   MemberController.deleteMember(req, res);
 });
-// admin delete member
-router.delete("/admin/delete/:memberId", (req, res) => {
+
+// Admin delete a member
+router.delete('/admin/delete/:memberId', (req, res) => {
   MemberController.adminDeleteMember(req, res);
 });
-// serach user by member id
-router.get("/reserche/:handle", (req, res) => {
+
+// Search for a user by handle
+router.get('/reserche/:handle', (req, res) => {
   MemberController.searchMember(req, res);
 });
-// update member profile picture
+
+// Update a member's profile picture
 router.put(
-  "/avatarUpload/:memberId",
-  resources.single("avatar"),
+  '/avatarUpload/:memberId',
+  resources.single('avatar'),
   (req, res) => {
     MemberController.createAvatarController(req, res);
-  }
+  },
 );
 
-// update member cover picture
+// Update a member's cover picture
 router.put(
-  "/coverImageUpload/:memberId",
-  resources.single("coverImage"),
+  '/coverImageUpload/:memberId',
+  resources.single('coverImage'),
   (req, res) => {
     MemberController.createCoverImageController(req, res);
-  }
+  },
 );
 
 module.exports = router;
