9be09f2dd3c7ccd3831b1596f4aa0edc864aed64
borisngong <borisngong@gmail>
2025-01-06 23:44:27 -0800

    Utility for creating file URLs from enironmental variables

--- BoroHubMedia_Backend/coreUtils/create-fileUrl.js
+++ BoroHubMedia_Backend/coreUtils/create-fileUrl.js
@@ -3,6 +3,7 @@
  * @param {string} filename - The name of the file to create the URL for
  * @returns {string} The full file URL as a string
  */
-const createFileUrl = (filename) => `${process.env.UPLOADS_BASE_URL}/media/images/${filename}`;
+const createFileUrl = (filename) =>
+  `${process.env.UPLOADS_BASE_URL}/media/images/${filename}`;
 
 module.exports = { createFileUrl };
