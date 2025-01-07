34aa735c21603e557a14785d7b62352c71cb0508
borisngong <borisngong@gmail>
2025-01-06 23:39:17 -0800

    Utility for generatin file URLs

--- BoroHubMedia_Backend/coreUtils/create-fileUrl.js
+++ BoroHubMedia_Backend/coreUtils/create-fileUrl.js
@@ -1,7 +1,8 @@
-const express = require("express");
-
-const createFileUrl = (filename) => {
-  return `${process.env.UPLOADS_BASE_URL}/media/images/${filename}`;
-};
+/**
+ * Creates a file URL by combining the base URL from environment variables
+ * @param {string} filename - The name of the file to create the URL for
+ * @returns {string} The full file URL as a string
+ */
+const createFileUrl = (filename) => `${process.env.UPLOADS_BASE_URL}/media/images/${filename}`;
 
 module.exports = { createFileUrl };
