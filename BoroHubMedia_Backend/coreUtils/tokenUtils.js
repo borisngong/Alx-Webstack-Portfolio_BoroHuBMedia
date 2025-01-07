57a3c85852415af4c90b5ccff522aea97dfc72b9
borisngong <borisngong@gmail>
2025-01-06 23:54:25 -0800

    Utility function for generating access and refresh tokens

--- BoroHubMedia_Backend/coreUtils/tokenUtils.js
+++ BoroHubMedia_Backend/coreUtils/tokenUtils.js
@@ -1,5 +1,11 @@
 const jwt = require("jsonwebtoken");
 
+/**
+ * Responsible for generating an access token for a member
+ * @function generateAccessToken
+ * @param {Object} member - The member object containing `_id` and `handle`
+ * @returns {string} - A signed JWT access token containing the member's ID and handle
+ */
 const generateAccessToken = (member) => {
   return jwt.sign(
     { id: member._id, handle: member.handle },
@@ -8,6 +14,12 @@ const generateAccessToken = (member) => {
   );
 };
 
+/**
+ * Responsible for generating a refresh token for a member
+ * @function generateRefreshToken
+ * @param {Object} member - The member object containing `_id`
+ * @returns {string} - A signed JWT refresh token containing the member's ID
+ */
 const generateRefreshToken = (member) => {
   return jwt.sign({ id: member._id }, process.env.REFRESH_JWT, {
     expiresIn: process.env.EXP_JWT,
