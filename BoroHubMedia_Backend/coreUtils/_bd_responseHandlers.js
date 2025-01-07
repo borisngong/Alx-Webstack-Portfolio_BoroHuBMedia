04769b761cebc6458e65ea69103a4fd83109e01d
borisngong <borisngong@gmail>
2025-01-06 23:33:39 -0800

    Success and Error response handler functions for API endpoints

--- BoroHubMedia_Backend/coreUtils/_bd_responseHandlers.js
+++ BoroHubMedia_Backend/coreUtils/_bd_responseHandlers.js
@@ -1,4 +1,9 @@
-const express = require("express");
+/**
+ * Responsible for sending success responses to the client with the data and status code
+ * @param {*} res  - The response object
+ * @param {*} data - The data to send
+ * @param {*} statusCode - The status code to send (default is 200)
+ */
 const sendSuccessResponse = (res, data, statusCode = 200) => {
   res.status(statusCode).json({
     success: true,
@@ -6,11 +11,16 @@ const sendSuccessResponse = (res, data, statusCode = 200) => {
   });
 };
 
-// Function to send error responses
+/**
+ * Responsible for sending error responses to the client with the error message and status code
+ * @param {*} res  - The response object
+ * @param {*} error - The error object
+ * @param {*} statusCode - The status code to send
+ */
 const sendErrorResponse = (res, error, statusCode = 400) => {
   res.status(statusCode).json({
     success: false,
-    message: error.message || "Internal Server Error",
+    message: error.message || 'Internal Server Error',
     error: error.stack || error,
   });
 };
