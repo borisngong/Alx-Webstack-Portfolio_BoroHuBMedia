ac17406e6b4617953e543bd4752de63da55dd6bf
borisngong <borisngong@gmail>
2025-01-07 01:48:45 -0800

    Middleware for handling media file uploads

--- BoroHubMedia_Backend/middlewares/mediaUploads.js
+++ BoroHubMedia_Backend/middlewares/mediaUploads.js
@@ -1,13 +1,22 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
 
-// Configure Multer storage
+/**
+ * Multer configuration that handles file uploads
+ * - Configures disk storage for uploaded files
+ * - Saves files to the `media/images` directory
+ * - Generates unique filenames using the fieldname, current timestamp, and original file extension
+ * @constant {Object} resources
+ * @property {Object} storage - Disk storage configuration for Multer.
+ * @property {Function} storage.destination - Specifies the directory to save uploaded files
+ * @property {Function} storage.filename - Generates a unique filename for each uploaded file
+ */
 const resources = multer({
   storage: multer.diskStorage({
-    destination: "media/images",
+    destination: 'media/images',
     filename: (req, file, cb) => {
       const uniqueName = `${file.fieldname}-${Date.now()}${path.extname(
-        file.originalname
+        file.originalname,
       )}`;
       cb(null, uniqueName);
     },
