43b64ccf8137494a5afbfb0873eefdcaffcf4a76
borisngong <borisngong@gmail>
2025-01-07 17:28:31 -0800

    Project's  main entry point for the application

--- BoroHubMedia_Backend/_bdmainEntry.js
+++ BoroHubMedia_Backend/_bdmainEntry.js
@@ -1,11 +1,10 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const { setupServer, boroHubMediaAPIDB } = require("./serverCore");
 
 // Load environment variables if not in test environment
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   dotenv.config();
 }
-
-const { setupServer, boroHubMediaAPIDB } = require('./serverCore');
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,16 +19,13 @@ const startServerInstance = () => {
   appInstance.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
     console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
-    console.log(
-      `Postman Docs available at http://localhost:${PORT}/postman-docs`,
-    );
   });
 
   return appInstance;
 };
 
 // Start the server only if not in test environment
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   startServerInstance();
 }
 
