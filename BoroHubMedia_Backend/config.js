e058152ffe63e1308719c8559b6f1971f46a815c
borisngong <borisngong@gmail>
2025-01-21 18:25:33 -0800

    Add configuration for server settings, media path, JWT secret, and database URL.

--- BoroHubMedia_Backend/config.js
+++ BoroHubMedia_Backend/config.js
@@ -1,8 +1,8 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   port: process.env.PORT || 3000,
-  mediaPath: process.env.MEDIA_PATH || path.join(__dirname, "media/images"),
+  mediaPath: process.env.MEDIA_PATH || path.join(__dirname, 'media/images'),
   jwtSecret: process.env.JWT_SECRET,
   databaseUrl: process.env.DB_URI,
 };
