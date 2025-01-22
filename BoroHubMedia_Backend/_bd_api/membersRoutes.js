951053215cf446bc280db78c73ab9cac9ef02b92
borisngong <borisngong@gmail>
2025-01-21 18:16:10 -0800

    Updated member routes to include CRUD operations, follow/unfollow, and media uploads

--- BoroHubMedia_Backend/_bd_api/membersRoutes.js
+++ BoroHubMedia_Backend/_bd_api/membersRoutes.js
@@ -5,63 +5,67 @@ const resources = require('../middlewares/mediaUploads');
 
 const router = express.Router();
 
+// refresh token
+router.post('/refresh-token', authenticateToken, (req, res) => {
+  MemberController.refreshAccessToken(req, res);
+});
 // Delete a member with admin privileges
 router.delete('/delete/:memberId', authenticateToken, isAdmin, (req, res) => {
   MemberController.adminDeleteMember(req, res);
 });
 
 // Retrieve a member's details
-router.get('/:memberId', (req, res) => {
+router.get('/:memberId', authenticateToken, (req, res) => {
   MemberController.getMember(req, res);
 });
 
 // Update a member's details
-router.put('/update/:memberId', (req, res) => {
+router.put('/update/:memberId', authenticateToken, (req, res) => {
   MemberController.updateMember(req, res);
 });
 
-// Delete a member's details
-router.delete('/delete/:memberId', (req, res) => {
-  MemberController.deleteMember(req, res);
-});
-
 // Follow a member
-router.put('/follow/:memberId', (req, res) => {
+router.put('/follow/:memberId', authenticateToken, (req, res) => {
   MemberController.followMember(req, res);
 });
 
 // Unfollow a member
-router.delete('/unfollow/:memberId', (req, res) => {
+router.delete('/unfollow/:memberId', authenticateToken, (req, res) => {
   MemberController.unfollowMember(req, res);
 });
 
 // Get a list of followers
-router.get('/followers/:memberId', (req, res) => {
+router.get('/followers/:memberId', authenticateToken, (req, res) => {
   MemberController.getFollowersList(req, res);
 });
 
 // Restrict a user
-router.post('/restricted/:memberId', (req, res) => {
+router.post('/restricted/', authenticateToken, (req, res) => {
   MemberController.restrictedMember(req, res);
 });
 
 // Unrestrict a user
-router.delete('/unrestricted/:memberId', (req, res) => {
+router.delete('/unrestricted/', authenticateToken, (req, res) => {
   MemberController.unrestrictedMember(req, res);
 });
 
 // Get a list of blocked users
-router.get('/restricting/:memberId', (req, res) => {
-  MemberController.getRestrictedList(req, res);
-});
+router.get(
+  '/restricted-list/:memberId',
+  authenticateToken,
+  isAdmin,
+  (req, res) => {
+    MemberController.getRestrictedList(req, res);
+  },
+);
 
 // Delete a member
-router.delete('/delete/:memberId', (req, res) => {
+router.delete('/delete/:memberId', authenticateToken, (req, res) => {
   MemberController.deleteMember(req, res);
 });
 
 // Admin delete a member
-router.delete('/admin/delete/:memberId', (req, res) => {
+router.delete('/admin/delete/:memberId', authenticateToken, (req, res) => {
   MemberController.adminDeleteMember(req, res);
 });
 
@@ -71,8 +75,9 @@ router.get('/reserche/:handle', (req, res) => {
 });
 
 // Update a member's profile picture
-router.put(
-  '/avatarUpload/:memberId',
+router.post(
+  '/avatarUpload/',
+  authenticateToken,
   resources.single('avatar'),
   (req, res) => {
     MemberController.createAvatarController(req, res);
@@ -81,7 +86,8 @@ router.put(
 
 // Update a member's cover picture
 router.put(
-  '/coverImageUpload/:memberId',
+  '/coverImageUpload/',
+  authenticateToken,
   resources.single('coverImage'),
   (req, res) => {
     MemberController.createCoverImageController(req, res);
