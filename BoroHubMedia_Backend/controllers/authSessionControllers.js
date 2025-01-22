fd24993542ddfbecbfa997430af83fc0964d3332
borisngong <borisngong@gmail>
2025-01-21 18:36:48 -0800

    Update MemberAuthenticationController for account initialization, authentication, session management, and token handling, and validations

--- BoroHubMedia_Backend/controllers/authSessionControllers.js
+++ BoroHubMedia_Backend/controllers/authSessionControllers.js
@@ -3,6 +3,9 @@ const jwt = require('jsonwebtoken');
 const Member = require('../coreModels/memberSchema');
 const { BDERROR } = require('../middlewares/handleErrors');
 const {
+  initializeAccountSchema,
+} = require('../coreModels/schemas/initializeAccountSchema');
+const {
   sendSuccessResponse,
   sendErrorResponse,
 } = require('../coreUtils/_bd_responseHandlers');
@@ -13,38 +16,32 @@ const {
 const { sanitizeMemberData } = require('../coreUtils/sanitized');
 
 /**
- * MemberController class to manage user-related operations
+ * MemberAuthenticationController class to manage member-related operations
  */
-class MemberController {
+class MemberAuthenticationController {
   /**
-   * initializeAccount - Responsible for initializing a new user account
-   * @param {Object} req - representsrequest object
-   * @param {Object} res - respresents the response object
-   * @param {Function} next - represents next middleware function
+   * initializeAccount - Responsible for initializing a new member account
+   * @param {Object} req - Represents request object
+   * @param {Object} res - Represents the response object
    */
   static async initializeAccount(req, res) {
     try {
+      // Validate input using Joi schema
       const {
-        hashedPassword, emailAddress, handle, fullName, aboutMe, role,
-      } = req.body;
+        plainPassword, emailAddress, handle, fullName, aboutMe, role,
+      } = await initializeAccountSchema.validateAsync(req.body);
 
-      // Validate input fields
-      if (!hashedPassword || !emailAddress || !handle || !fullName) {
-        throw new BDERROR('All fields must be filled out', 400);
-      }
-
-      // Check for existing user with the same email or handle
+      // Check for existing member with the same email or handle
       const existingUser = await Member.findOne({
         $or: [{ emailAddress }, { handle }],
       });
+
       if (existingUser) {
         throw new BDERROR('Email or handle is already taken', 400);
       }
 
-      // Hash the plain password using bcrypt
-      const bcryptHashedPassword = await bcrypt.hash(hashedPassword, 10);
-
-      // Create a new user
+      // Hash the password and create a new member
+      const bcryptHashedPassword = await bcrypt.hash(plainPassword, 10);
       const newUser = new Member({
         fullName,
         handle,
@@ -55,14 +52,12 @@ class MemberController {
       });
 
       const savedUser = await newUser.save();
-      // Sanitize member data to return without the password
-      const sanitizedMember = sanitizeMemberData(savedUser);
 
       return sendSuccessResponse(
         res,
         {
-          message: 'Member account created succesfully!',
-          savedUser: sanitizedMember,
+          message: 'Member account initialized successfully!',
+          member: sanitizeMemberData(savedUser),
         },
         201,
       );
@@ -72,14 +67,19 @@ class MemberController {
   }
 
   /**
-   * accessAccount - Responsible for authenticating a user and returns access and refresh tokens
+   * accessAccount - Responsible for authenticating a member and returns access and refresh tokens
    * @param {Object} req - Represents the request object
    * @param {Object} res - Represents the response object
    */
   static async accessAccount(req, res) {
-    const { emailAddress, handle, hashedPassword } = req.body;
+    const { emailAddress, handle, plainPassword } = req.body;
 
     try {
+      if (!emailAddress && !handle) {
+        throw new BDERROR('Email or handle is required to access account', 400);
+      }
+
+      // Ensure at least one of the email or handle is valid
       const member = await Member.findOne({
         $or: [{ emailAddress }, { handle }],
       });
@@ -87,19 +87,21 @@ class MemberController {
       if (!member) {
         return sendErrorResponse(
           res,
-          new BDERROR('Registered member not found', 404),
+          new BDERROR('Input details incorrect', 401),
         );
       }
-      // check for wrong account details
-      if (hashedPassword !== member.hashedPassword) {
-        throw new BDERROR('Input details incorrect', 401);
+
+      if (!plainPassword) {
+        throw new BDERROR('Password is required to access account', 400);
       }
 
-      const pwMatch = await bcrypt.compare(
-        hashedPassword,
+      // Compare the hashed password with the provided password
+      const isValidPassword = await bcrypt.compare(
+        plainPassword,
         member.hashedPassword,
       );
-      if (!pwMatch) {
+
+      if (!isValidPassword) {
         return sendErrorResponse(
           res,
           new BDERROR('Input details incorrect', 401),
@@ -109,12 +111,28 @@ class MemberController {
       const accessToken = generateAccessToken(member);
       const refreshToken = generateRefreshToken(member);
 
+      if (
+        req.cookies.accessToken
+        && req.cookies.accessToken.expires < Date.now()
+      ) {
+        // Refresh the access token
+        const newAccessToken = await this.refreshAccessToken(req, res);
+        return sendSuccessResponse(
+          res,
+          {
+            message: 'Access token refreshed successfully!',
+            accessToken: newAccessToken,
+          },
+          200,
+        );
+      }
+
       // Set the access token in an HttpOnly cookie
       res.cookie('accessToken', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'None',
-        maxAge: 1 * 60 * 60 * 1000, // 1 hour
+        maxAge: 1 * 60 * 60 * 1000,
       });
 
       // Set refresh token in a cookie
@@ -122,20 +140,19 @@ class MemberController {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'None',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
       // Sanitize member data to return without the password
-
       const sanitizedMember = sanitizeMemberData(member);
 
       return sendSuccessResponse(
         res,
         {
-          message: 'Member account accessed succesfully!',
+          message: 'Member account accessed successfully!',
           member: sanitizedMember,
         },
-        201,
+        200,
       );
     } catch (error) {
       return sendErrorResponse(res, error, error.statusCode || 500);
@@ -143,24 +160,19 @@ class MemberController {
   }
 
   /**
-   * endSession - responsible for ending the member session by clearing cookies
-   * @param {Object} req - represnts request object
-   * @param {Object} res - reoresnts response object
-   * @param {Function} next -  Responsible for calling the next middleware function
+   * endSession - Responsible for ending the member session by clearing cookies
+   * @param {Object} req - Represents request object
+   * @param {Object} res - Represents response object
    */
   static async endSession(req, res) {
     try {
+      if (!req.cookies.accessToken) {
+        throw new BDERROR('No session to end', 400);
+      }
+
       // Clear the access token and refresh token cookies
-      res.clearCookie('accessToken', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'None',
-      });
-      res.clearCookie('refreshToken', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'None',
-      });
+      res.clearCookie('accessToken');
+      res.clearCookie('refreshToken');
 
       return sendSuccessResponse(
         res,
@@ -175,49 +187,49 @@ class MemberController {
   }
 
   /**
-   * getUserSession - Responsible for retrieving the current member's session information.
-   * @param {Object} req - represnts the request object
-   * @param {Object} res - respresents the response object
-   * @param {Function} next - responsible for calling the next middleware function
+   * getUser Session - Responsible for retrieving the current member's session information
+   * @param {Object} req - Represents the request object
+   * @param {Object} res - Represents the response object
    */
-  static async getUserSession(req, res) {
-    const token = req.cookies.accessToken; // Access the token from cookies
+  static async getMemberSession(req, res) {
+    const token = req.cookies.accessToken;
 
-    // Check if the token exists
     if (!token) {
-      throw new BDERROR('No token present in cookies', 401);
+      throw new BDERROR('No session/token found, please log in', 404);
     }
 
-    // Verify the token
-    jwt.verify(token, process.env.SKEY_JWT, async (err, data) => {
-      if (err) {
-        throw new BDERROR('Invalid token or not present in cookies', 401);
+    try {
+      // Verify the token
+      const data = jwt.verify(token, process.env.SKEY_JWT);
+      const { id } = data;
+      const member = await Member.findOne({ _id: id });
+
+      if (!member) {
+        throw new BDERROR('Member not found', 404);
       }
 
-      try {
-        const { id } = data;
-        const member = await Member.findOne({ _id: id });
+      // Sanitize member data to return without the password
+      const sanitizedMember = sanitizeMemberData(member);
 
-        if (!member) {
-          throw new BDERROR('Member can be found', 404);
-        }
-
-        // Sanitize member data to return with the password
-        const sanitizedMember = sanitizeMemberData(member);
-
-        return sendSuccessResponse(
-          res,
-          {
-            message: 'Current member session gotten succesfully!',
-            member: sanitizedMember,
-          },
-          201,
-        );
-      } catch (error) {
-        return sendErrorResponse(res, error, error.statusCode || 500);
+      return sendSuccessResponse(
+        res,
+        {
+          message: 'Current member session retrieved successfully!',
+          member: sanitizedMember,
+        },
+        200,
+      );
+    } catch (error) {
+      if (error instanceof BDERROR) {
+        return sendErrorResponse(res, error, error.statusCode);
       }
-    });
+      return sendErrorResponse(
+        res,
+        new BDERROR('Invalid token, please you must be logged in', 401),
+        401,
+      );
+    }
   }
 }
 
-module.exports = MemberController;
+module.exports = MemberAuthenticationController;
