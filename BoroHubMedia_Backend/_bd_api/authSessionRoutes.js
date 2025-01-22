a524ec4f1da26c4341909c1bc6ef54a1a53c9346
borisngong <borisngong@gmail>
2025-01-21 16:45:45 -0800

    Updated auth routes implementation  for member intiliasation,  access acount, endsession and get curretn member session

--- BoroHubMedia_Backend/_bd_api/authSessionRoutes.js
+++ BoroHubMedia_Backend/_bd_api/authSessionRoutes.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const MemberAuthenticationController = require('../controllers/authSessionControllers');
-const { authenticateToken } = require('../middlewares/authIsAdmin');
-const { accessLimiter } = require('../middlewares/rateLimiter');
+const express = require("express");
+const MemberAuthenticationController = require("../controllers/authSessionControllers");
+const { authenticateToken } = require("../middlewares/authIsAdmin");
+const { accessLimiter } = require("../middlewares/rateLimiter");
 
 const router = express.Router();
 
-// Member Auth Route
-router.post('/initializeAccount', (req, res) => {
+// Member Auth Routes
+router.post("/initializeAccount", (req, res) => {
   MemberAuthenticationController.initializeAccount(req, res);
 });
-router.post('/accessAccount', accessLimiter, (req, res) => {
+router.post("/accessAccount", accessLimiter, (req, res) => {
   MemberAuthenticationController.accessAccount(req, res);
 });
-router.get('/endSession', authenticateToken, (req, res) => {
+router.get("/endSession", authenticateToken, (req, res) => {
   MemberAuthenticationController.endSession(req, res);
 });
-router.get('/getMemberSession', authenticateToken, (req, res) => {
+router.get("/getMemberSession", authenticateToken, (req, res) => {
   MemberAuthenticationController.getMemberSession(req, res);
 });
 
