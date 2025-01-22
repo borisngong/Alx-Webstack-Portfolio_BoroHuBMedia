086e85e2587c31333a0506809eeed1b0405b6770
borisngong <borisngong@gmail>
2025-01-22 08:21:13 -0800

    Updated auth routes implementation  for member intiliasation,  access acount, endsession and get current member session

--- BoroHubMedia_Backend/_bd_api/authSessionRoutes.js
+++ BoroHubMedia_Backend/_bd_api/authSessionRoutes.js
@@ -6,16 +6,24 @@ const { accessLimiter } = require("../middlewares/rateLimiter");
 const router = express.Router();
 
 // Member Auth Routes
+
+// Initialize a new user account
 router.post("/initializeAccount", (req, res) => {
   MemberAuthenticationController.initializeAccount(req, res);
 });
+
+// Access existing user account (login)
 router.post("/accessAccount", accessLimiter, (req, res) => {
   MemberAuthenticationController.accessAccount(req, res);
 });
+
+// End user session (logout)
 router.get("/endSession", authenticateToken, (req, res) => {
   MemberAuthenticationController.endSession(req, res);
 });
-router.get("/getMemberSession", authenticateToken, (req, res) => {
+
+// Get current member session information
+router.get("/getSession", authenticateToken, (req, res) => {
   MemberAuthenticationController.getMemberSession(req, res);
 });
 
