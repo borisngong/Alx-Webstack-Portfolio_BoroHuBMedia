4a4782fc17d544d9ff9ffc7f7a90078fb1bea46a
borisngong <borisngong@gmail>
2025-01-29 07:35:54 -0800

    Success and Error response handler functions for API endpoints

--- BoroHubMedia_Backend/coreUtils/_bd_responseHandlers.js
+++ BoroHubMedia_Backend/coreUtils/_bd_responseHandlers.js
@@ -1,6 +1,8 @@
+const { BDERROR } = require('../middlewares/handleErrors');
+
 /**
  * Responsible for sending success responses to the client with the data and status code
- * @param {*} res  - The response object
+ * @param {*} res - The response object
  * @param {*} data - The data to send
  * @param {*} statusCode - The status code to send (default is 200)
  */
@@ -13,15 +15,28 @@ const sendSuccessResponse = (res, data, statusCode = 200) => {
 
 /**
  * Responsible for sending error responses to the client with the error message and status code
- * @param {*} res  - The response object
- * @param {*} error - The error object
- * @param {*} statusCode - The status code to send
+ * @param {Object} res - The response object
+ * @param {Error} error - The error object
+ * @param {number} [statusCode=400] - The status code to send
  */
 const sendErrorResponse = (res, error, statusCode = 400) => {
-  res.status(statusCode).json({
+  // Create a new BDERROR instance if necessary
+  let errorResponse = error;
+
+  if (!(errorResponse instanceof BDERROR)) {
+    errorResponse = new BDERROR(
+      error.message || 'Internal Server Error',
+      statusCode,
+    );
+  }
+
+  // Send the error response
+  res.status(errorResponse.status || statusCode).json({
     success: false,
-    message: error.message || 'Internal Server Error',
-    error: error.stack || error,
+    message: errorResponse.message || 'Internal Server Error',
+    status: errorResponse.status || statusCode,
+    stack:
+      process.env.NODE_ENV === 'development' ? errorResponse.stack : undefined, // Include stack trace in development only
   });
 };
 
