2bd84bb9a9bc98703a58032f3c42fb7cc056e333
borisngong <borisngong@gmail>
2025-02-06 02:38:51 -0800

    Add CORS support to allow cross-origin requests and enhance API accessibility

--- BoroHubMedia_Backend/middlewares/setupMiddleware.js
+++ BoroHubMedia_Backend/middlewares/setupMiddleware.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
 /**
  * Sets up middleware for an Express application
@@ -10,14 +11,18 @@ const helmet = require('helmet');
  * @returns {void}
  */
 const setupMiddleware = (app) => {
+  // Enable CORS
+  app.use(cors());
+
   // Enable logging using Morgan in development mode
-  app.use(morgan(':method :url :status :response-time ms'));
+  app.use(morgan(":method :url :status :response-time ms"));
 
   // Enable JSON parsing for request bodies
   app.use(express.json());
 
   // Parse cookies from request headers
   app.use(cookieParser());
+
   // Enable security headers using Helmet
   app.use(helmet());
 };
